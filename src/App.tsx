ef3366710767ab2f6d5564c894b11574848a0e35
MarkoVVV <sam.samlin@mail.ru>
2023-07-03 22:22:10 +0300

    fix imports in checkBox

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-import { useState } from 'react'
-
-import { Checkbox } from 'ui/checkBox/checkBox.tsx'
-
 export function App() {
-  const [check, setCheck] = useState(true)
-
-  const setChe = () => {
-    setCheck(!check)
-  }
-
-  console.log(check)
-
-  return (
-    <div>
-      <Checkbox onChange={() => setChe()} checked={check} disabled={false} />
-    </div>
-  )
+  return <div>Hello</div>
 }
