2b774292fece831c5414fafca9451052e7554b10
MarkoVVV <sam.samlin@mail.ru>
2023-07-06 19:13:27 +0300

    added 'header' component and 'avatar' component

---
+++ src/ui/header/header.stories.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ProfileAvatar } from 'assets/icons'
+import { Header } from 'ui/header/header.tsx'
+
+const meta = {
+  title: 'Header',
+  component: Header,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Header>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const LoggedIn: Story = {
+  args: {
+    user: {
+      name: 'Ivan',
+    },
+    isLogin: true,
+    children: <ProfileAvatar />,
+  },
+}
+
+export const LoggedOut: Story = {
+  render: args => {
+    const [isLogin, setIsLogin] = useState<boolean>(false)
+
+    const logined = () => {
+      setIsLogin(!isLogin)
+    }
+
+    return (
+      <>
+        <Header {...args} isLogin={isLogin} onLogin={logined} />
+      </>
+    )
+  },
+
+  args: {
+    user: {
+      name: 'Ivan',
+    },
+    children: <ProfileAvatar />,
+  },
+}
---
+++ src/ui/header/header.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react'
+
+import s from './header.module.scss'
+
+import { Logo } from 'assets/icons'
+import { Button } from 'ui/button'
+import { Avatars as Avatar } from 'ui/userAvatar/avatar.tsx'
+
+export type User = {
+  name: string
+  avatar?: string
+}
+
+interface HeaderProps {
+  user?: User
+  onLogin?: () => void
+  onLogout?: () => void
+  isLogin?: boolean
+  children?: ReactNode
+}
+
+export const Header = ({ user, onLogin, children, isLogin }: HeaderProps) => {
+  return (
+    <div className={s.header}>
+      <div className={s.logo}>
+        <Logo />
+      </div>
+      {isLogin ? (
+        <div className={s.userContainer}>
+          <div className={s.userName}>{user?.name}</div>
+          <Avatar src={user?.avatar}>{children}</Avatar>
+        </div>
+      ) : (
+        <Button onClick={onLogin}> Sign In </Button>
+      )}
+    </div>
+  )
+}
