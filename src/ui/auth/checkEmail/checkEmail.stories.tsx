3a7b3c06bc6416ddc33815609cb0f7c4df77f11d
Photi31 <photiniya@gmail.com>
2023-07-08 00:31:36 +0300

    Create Check Email component, style, stories.

---
+++ src/ui/auth/checkEmail/checkEmail.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { CheckEmail } from 'ui/auth/checkEmail/checkEmail.tsx'
+
+const meta = {
+  title: 'Components/CheckEmail',
+  component: CheckEmail,
+  tags: ['autodocs'],
+  argTypes: {
+    email: { control: 'text' },
+  },
+} satisfies Meta<typeof CheckEmail>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CheckEmailCard: Story = {
+  args: {
+    email: 'hhhahhd@gmail.com',
+  },
+}
---
+++ src/ui/auth/checkEmail/checkEmail.tsx
@@ -0,0 +1,32 @@
+import s from './checkEmail.module.scss'
+
+import { CheckEmailSvg } from 'assets/icons/checkEmail.tsx'
+import { Button } from 'ui/button'
+import { Card } from 'ui/card'
+import { Typography } from 'ui/typography'
+
+export type CheckEmailType = {
+  email: string
+}
+
+export const CheckEmail = ({ email }: CheckEmailType) => {
+  const classNames = {
+    root: s.root,
+    img: s.img,
+    email: s.email,
+  }
+
+  return (
+    <Card className={classNames.root}>
+      <Typography variant="large">Check Email</Typography>
+      <CheckEmailSvg className={classNames.img} />
+      <Typography variant="body2" color="inherit" className={classNames.email}>
+        We’ve sent an Email with instructions to <br />
+        {email}
+      </Typography>
+      <Button as="a" variant="primary">
+        Back to Sign In
+      </Button>
+    </Card>
+  )
+}
