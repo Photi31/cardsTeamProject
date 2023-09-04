831c1519d3171bfaf95c1ad31602642051c0646b
ksasrg <ksahtmlcss@gmail.com>
2023-09-04 19:04:14 +0300

    fix personal information stories upload avatar

--- src/components/profile/personal-information.stories.tsx
+++ src/components/profile/personal-information.stories.tsx
@@ -4,6 +4,9 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { PersonalInformation } from './personal-information.tsx'
 
+const defaultAva =
+  'https://sun9-2.userapi.com/impg/VeSbeev8_WQ8y4LGDdY1NcnvCNP_N5SatvHTUA/x9MdiRS1oKQ.jpg?size=200x200&quality=96&sign=3ef6f497a6419e576f033d6684b8afee'
+
 const meta = {
   title: 'Profile/Personal information',
   component: PersonalInformation,
@@ -16,28 +19,38 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: () => {
     const [name, setName] = useState('Ivan')
-    const [newAvatar, setNewAvatar] = useState<string | undefined>(
-      'https://sun9-2.userapi.com/impg/VeSbeev8_WQ8y4LGDdY1NcnvCNP_N5SatvHTUA/x9MdiRS1oKQ.jpg?size=200x200&quality=96&sign=3ef6f497a6419e576f033d6684b8afee&type=album'
-    )
+    const [newFile, setNewFile] = useState<File>()
+    const [newAvatar, setNewAvatar] = useState<string>('')
 
     const handleNameChange = (newName: string) => {
       setName(newName)
     }
-    const handleAvatarChange = (newAvatar: string) => {
-      setNewAvatar(newAvatar)
+
+    const handleAvatarChange = (newFile: File) => {
+      setNewFile(newFile)
+    }
+
+    const reader = new FileReader()
+
+    reader.onload = () => {
+      const result = reader.result as string
+
+      setNewAvatar(result)
+    }
+
+    if (newFile) {
+      reader.readAsDataURL(newFile)
     }
 
     return (
-      <>
-        <PersonalInformation
-          onAvatarChange={handleAvatarChange}
-          email={'j&johnson@gmail.com'}
-          name={name}
-          onNameChange={handleNameChange}
-          avatar={newAvatar}
-          onLogout={() => {}}
-        />
-      </>
+      <PersonalInformation
+        onAvatarChange={handleAvatarChange}
+        email={'j&johnson@gmail.com'}
+        name={name}
+        onNameChange={handleNameChange}
+        avatar={newAvatar || defaultAva}
+        onLogout={() => {}}
+      />
     )
   },
 
